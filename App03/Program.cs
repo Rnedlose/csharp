f713438167f299b5cbfef5f8719ac2a22a7621e0
Rnedlose <nedloserodney@gmail.com>
2019-08-12 22:00:49 -0500

    Week 02

---
+++ App03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]  Nums = {2,5,23,98,100,456,23};
+
+            Console.WriteLine("The total sum of all the array elements is: " + SumOfArray(Nums));
+        }
+        static int SumOfArray(int[] Arr)
+        {
+            int Total = 0;
+            foreach (int i in Arr)
+            {
+                Total += i;
+            }
+            return Total;
+        }
+    }
+}
