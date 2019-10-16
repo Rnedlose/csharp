004f408f352e36187a91e8622b175137b0552193
Rnedlose <nedloserodney@gmail.com>
2019-10-15 20:48:42 -0500

    Try-Catch Program

--- try_catch/Program.cs
+++ try_catch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System;   
+using System.Net.Mail;
 
 namespace try_catch
 {
@@ -6,27 +7,24 @@ namespace try_catch
     {
         static void Main()
         {
-            double a;
-            double b;
-            double c;
-
+            int a, b, c;
             string firstNum;
             string secondNum;
             string choice;
-
+      
             try
             {
-                Console.WriteLine("Let's try a math problem.");
+                Console.WriteLine("Let's try some math problems, then add them to an array.");
                 Console.WriteLine("Enter two numbers, and then choose operator.");
                 Console.WriteLine("\n\n");
 
-                               
+                            
                 Console.Write("Enter first number: ");
-                a = double.Parse(firstNum = Console.ReadLine());
+                a = int.Parse(firstNum = Console.ReadLine());
                 Console.WriteLine("\n\n");
 
                 Console.Write("Enter the second number: ");
-                b = double.Parse(secondNum = Console.ReadLine());
+                b = int.Parse(secondNum = Console.ReadLine());
                 Console.WriteLine("\n\n");
                 
                 Console.WriteLine("Now choose operator from below list.");
@@ -66,16 +64,61 @@ namespace try_catch
                     throw new InvalidChoiceException(choice);
                 }
             }
-            catch (ArithmeticException ex)
+            catch (DivideByZeroException ex)
             {
-                Console.WriteLine("Aritmetic Exception.");
+                Console.WriteLine("Cannot divide by zero.");
                 Console.WriteLine(ex.Message);
             }
             catch (FormatException ex)
             {
                 Console.WriteLine("Invalid.");
                 Console.WriteLine(ex.Message);
+            }catch (InvalidChoiceException ex) 
+            {
+                Console.WriteLine("Not a valid choice.");
+                Console.WriteLine(ex.Message);
             }
+            try
+            {
+                Console.WriteLine("Choose a number 1-9 to see that index in an array.");
+                int[] nums = {14,26,32,49,51,67,79,84,96};
+                int num;
+
+                num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The number at index " + num + " is: " + nums[num]);                  
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("That index is out of bounds.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidChoiceException ex)
+            {
+                Console.WriteLine("Not a valid choice.");
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine("Emails must be in format: username@host.*** ");
+                Console.Write("Enter an email address to validate: ");
+                string emailAddress = Console.ReadLine();
+                MailAddress testEmail = new MailAddress(emailAddress);
+                Console.WriteLine("Email Validated!");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Address is null!: " + ex.Message + "\n");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("Address is empty!: " + ex.Message + "\n");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("The address is not in the correct format!: " + ex.Message + "\n");
+            }
+            
         }
     
 
@@ -85,13 +128,13 @@ namespace try_catch
             {
 
             }
-
+                
             public InvalidChoiceException (string message)
                 : base(message)
             {
 
             }
-
+                
             public InvalidChoiceException (string message, Exception inner)
                 : base(message, inner)
             {
